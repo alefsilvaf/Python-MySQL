596c134c468d933071fa5e79d1f014fae0db32d7
Álef <54692623+aleffeels@users.noreply.github.com>
2020-08-07 17:54:50 -0300

    Add files via upload

---
+++ SQLPart.sql
@@ -0,0 +1,136 @@
+-- MySQL dump 10.13  Distrib 8.0.21, for Win64 (x86_64)
+--
+-- Host: localhost    Database: escola_curso
+-- ------------------------------------------------------
+-- Server version	8.0.21
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!50503 SET NAMES utf8 */;
+/*!40103 SET @OLD_TIME_ZONE=@@TIME_ZONE */;
+/*!40103 SET TIME_ZONE='+00:00' */;
+/*!40014 SET @OLD_UNIQUE_CHECKS=@@UNIQUE_CHECKS, UNIQUE_CHECKS=0 */;
+/*!40014 SET @OLD_FOREIGN_KEY_CHECKS=@@FOREIGN_KEY_CHECKS, FOREIGN_KEY_CHECKS=0 */;
+/*!40101 SET @OLD_SQL_MODE=@@SQL_MODE, SQL_MODE='NO_AUTO_VALUE_ON_ZERO' */;
+/*!40111 SET @OLD_SQL_NOTES=@@SQL_NOTES, SQL_NOTES=0 */;
+
+--
+-- Table structure for table `alunos`
+--
+
+DROP TABLE IF EXISTS `alunos`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!50503 SET character_set_client = utf8mb4 */;
+CREATE TABLE `alunos` (
+  `idalunos` int NOT NULL AUTO_INCREMENT,
+  `nome` varchar(100) NOT NULL,
+  `nascimento` date NOT NULL,
+  `endereco` varchar(150) NOT NULL,
+  `cidade` varchar(45) NOT NULL,
+  `estado` varchar(2) NOT NULL,
+  `cpf` varchar(11) NOT NULL,
+  PRIMARY KEY (`idalunos`)
+) ENGINE=InnoDB AUTO_INCREMENT=18 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `alunos`
+--
+
+LOCK TABLES `alunos` WRITE;
+/*!40000 ALTER TABLE `alunos` DISABLE KEYS */;
+INSERT INTO `alunos` VALUES (1,'Alef','1999-10-22','Rua da Cadelas','Guaranésia','MG','1212312321'),(2,'Lucas','1999-10-23','Rua das Cadelas','Guaranésia','MG','21321312321'),(3,'Joaquim','1000-10-23','Rua das Fadas','Guarana','UK','21312312312'),(4,'Amanda','2001-05-21','Rua das Fodas','tupi guarani','SP','324324'),(5,'Lara Milena','2000-12-12','Não sei','Paris','FR','21312312123'),(6,'Sarah Araujo','2000-12-12','Não sei','Bahia','BR','432432'),(7,'Welsey Toledo','2001-12-12','Não sei','Mato','BR','234234423'),(13,'Labaxurias Urias','4040-01-01','Dinhabawns','Diabatown','SP','66666666666');
+/*!40000 ALTER TABLE `alunos` ENABLE KEYS */;
+UNLOCK TABLES;
+
+--
+-- Table structure for table `alunos_cursos`
+--
+
+DROP TABLE IF EXISTS `alunos_cursos`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!50503 SET character_set_client = utf8mb4 */;
+CREATE TABLE `alunos_cursos` (
+  `id_alunos_cursos` int NOT NULL AUTO_INCREMENT,
+  `id_aluno` int NOT NULL,
+  `id_curso` int NOT NULL,
+  PRIMARY KEY (`id_alunos_cursos`),
+  KEY `ik_aluno_cursos_idx` (`id_aluno`),
+  KEY `chave_id_curso_idx` (`id_curso`),
+  CONSTRAINT `chave_id_aluno` FOREIGN KEY (`id_aluno`) REFERENCES `alunos` (`idalunos`),
+  CONSTRAINT `chave_id_curso` FOREIGN KEY (`id_curso`) REFERENCES `cursos` (`idcursos`)
+) ENGINE=InnoDB AUTO_INCREMENT=9 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `alunos_cursos`
+--
+
+LOCK TABLES `alunos_cursos` WRITE;
+/*!40000 ALTER TABLE `alunos_cursos` DISABLE KEYS */;
+INSERT INTO `alunos_cursos` VALUES (1,1,1),(2,1,2),(3,2,1),(4,2,3),(5,3,1),(6,3,2),(7,4,1),(8,5,1);
+/*!40000 ALTER TABLE `alunos_cursos` ENABLE KEYS */;
+UNLOCK TABLES;
+
+--
+-- Table structure for table `cursos`
+--
+
+DROP TABLE IF EXISTS `cursos`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!50503 SET character_set_client = utf8mb4 */;
+CREATE TABLE `cursos` (
+  `idcursos` int NOT NULL AUTO_INCREMENT,
+  `nome` varchar(100) NOT NULL,
+  PRIMARY KEY (`idcursos`)
+) ENGINE=InnoDB AUTO_INCREMENT=4 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `cursos`
+--
+
+LOCK TABLES `cursos` WRITE;
+/*!40000 ALTER TABLE `cursos` DISABLE KEYS */;
+INSERT INTO `cursos` VALUES (1,'Codigniter'),(2,'Python'),(3,'MySQL');
+/*!40000 ALTER TABLE `cursos` ENABLE KEYS */;
+UNLOCK TABLES;
+
+--
+-- Table structure for table `notas`
+--
+
+DROP TABLE IF EXISTS `notas`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!50503 SET character_set_client = utf8mb4 */;
+CREATE TABLE `notas` (
+  `id_nota` int NOT NULL AUTO_INCREMENT,
+  `id_aluno_curso` int NOT NULL,
+  `descricao_atividades` varchar(45) NOT NULL,
+  `vlr_nota` decimal(5,2) NOT NULL,
+  PRIMARY KEY (`id_nota`),
+  KEY `fk_ac_idx` (`id_aluno_curso`)
+) ENGINE=InnoDB AUTO_INCREMENT=12 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `notas`
+--
+
+LOCK TABLES `notas` WRITE;
+/*!40000 ALTER TABLE `notas` DISABLE KEYS */;
+INSERT INTO `notas` VALUES (1,1,'Prova 1',28.00),(2,3,'Prova 1',25.00),(3,5,'Prova 1',28.00),(4,2,'Exercicio 1',10.00),(5,6,'Exercicio 1',12.00),(6,1,'Prova 2',22.00),(7,2,'Prova 2',20.00);
+/*!40000 ALTER TABLE `notas` ENABLE KEYS */;
+UNLOCK TABLES;
+/*!40103 SET TIME_ZONE=@OLD_TIME_ZONE */;
+
+/*!40101 SET SQL_MODE=@OLD_SQL_MODE */;
+/*!40014 SET FOREIGN_KEY_CHECKS=@OLD_FOREIGN_KEY_CHECKS */;
+/*!40014 SET UNIQUE_CHECKS=@OLD_UNIQUE_CHECKS */;
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
+/*!40111 SET SQL_NOTES=@OLD_SQL_NOTES */;
+
+-- Dump completed on 2020-08-07 17:53:11
